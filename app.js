b1bf563173bb821eec6c5253ec75e8d86c3a046d
NanuCareaga <nanu.careaga@gmail.com>
2023-06-07 18:47:10 -0300

    Trabajo Practico

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname,"/public")));
+
+app.get("/", (req,res)=>{
+    res.sendFile((__dirname + "/views/home.html"));
+}) ;
+
+app.get("*", (req,res)=>{
+    res.send("Ruta restringida, hemos localizado la direccion de su computador");
+});
+
+app.listen(3002, function () {
+    console.log("Servidor corriendo");
+});
